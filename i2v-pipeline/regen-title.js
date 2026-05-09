2de4ddf7e2081dee8148cf5e2017b390b90fd151
fuufuufuuf <75222787@qq.com>
2026-05-09 20:16:52 +0800

    update

--- i2v-pipeline/regen-title.js
+++ i2v-pipeline/regen-title.js
@@ -235,7 +235,9 @@ async function main() {
   }
 }
 
-main().catch((err) => {
-  console.error('[Fatal]', err.message);
-  process.exit(1);
-});
+if (require.main === module) {
+  main().catch((err) => {
+    console.error('[Fatal]', err.message);
+    process.exit(1);
+  });
+}
